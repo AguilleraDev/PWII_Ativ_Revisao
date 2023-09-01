bcc4ec83fac2830ffb84719d2419dd454d20564b
Lucas Aguillera <aguilleralucas@gmail.com>
2023-08-31 22:26:54 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListarprodutosComponent } from './components/produtos/listarprodutos/listarprodutos.component';
+import { CadastrarprodutoComponent } from './components/produtos/cadastrarproduto/cadastrarproduto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListarprodutosComponent,
+    CadastrarprodutoComponent
   ],
   imports: [
     BrowserModule,
